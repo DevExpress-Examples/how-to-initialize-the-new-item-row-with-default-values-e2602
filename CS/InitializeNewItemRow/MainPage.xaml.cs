d7e03e2fc007a065fd7a4b9da9d3c3df92999c28
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/InitializeNewItemRow/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+
+namespace InitializeNewItemRow {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+
+        private void TableView_InitNewRow(object sender, DevExpress.Xpf.Grid.InitNewRowEventArgs e) {
+            grid.SetCellValue(e.RowHandle, "UnitPrice", 10);
+            grid.SetCellValue(e.RowHandle, "Quantity", 10);
+            grid.SetCellValue(e.RowHandle, "Country", "USA");
+            grid.SetCellValue(e.RowHandle, "City", "NY");
+        }
+
+        private void TableView_ValidateRow(object sender, DevExpress.Xpf.Grid.GridRowValidationEventArgs e) {
+            if (e.Row == null) return;
+            if (e.RowHandle == GridControl.NewItemRowHandle)
+                e.IsValid = ((Product)e.Row).ProductName != string.Empty;
+        }
+
+        private void TableView_InvalidRowException(object sender, DevExpress.Xpf.Grid.InvalidRowExceptionEventArgs e) {
+            if (e.RowHandle == GridControl.NewItemRowHandle) {
+                e.ErrorText = "Please enter the Product name. ";
+                e.WindowCaption = "Input Error";
+                grid.View.FocusedColumn = grid.Columns["ProductName"];
+            }
+        }
+    }
+}
---
+++ VB/InitializeNewItemRow/MainPage.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+
+Namespace InitializeNewItemRow
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New ProductList()
+		End Sub
+
+		Private Sub TableView_InitNewRow(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.InitNewRowEventArgs)
+			grid.SetCellValue(e.RowHandle, "UnitPrice", 10)
+			grid.SetCellValue(e.RowHandle, "Quantity", 10)
+			grid.SetCellValue(e.RowHandle, "Country", "USA")
+			grid.SetCellValue(e.RowHandle, "City", "NY")
+		End Sub
+
+		Private Sub TableView_ValidateRow(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.GridRowValidationEventArgs)
+			If e.Row Is Nothing Then
+				Return
+			End If
+			If e.RowHandle = GridControl.NewItemRowHandle Then
+				e.IsValid = (CType(e.Row, Product)).ProductName <> String.Empty
+			End If
+		End Sub
+
+		Private Sub TableView_InvalidRowException(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.InvalidRowExceptionEventArgs)
+			If e.RowHandle = GridControl.NewItemRowHandle Then
+				e.ErrorText = "Please enter the Product name. "
+				e.WindowCaption = "Input Error"
+				grid.View.FocusedColumn = grid.Columns("ProductName")
+			End If
+		End Sub
+	End Class
+End Namespace
